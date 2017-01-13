a4149e8540b2dd98e932eba0eeeaacf7b59c0e46
khs300kr <khs300kr@naver.com>
2017-01-13 18:45:08 +0900

    1.13 (에코서버 작업2)

--- Practice_Project/[17.01.12]Echo Server_RingBuffer/[17.01.12]Echo Server_RingBuffer/TCP_Server.cpp
+++ Practice_Project/[17.01.12]Echo Server_RingBuffer/[17.01.12]Echo Server_RingBuffer/TCP_Server.cpp
@@ -6,7 +6,70 @@
 using namespace std;
 
 #define SERVERPORT 9000
-#define BUFSIZE 1024
+#define BUFSIZE 128
+//
+// Packet
+enum ECharType
+{
+	// Tanker
+	Rine,
+	Diva,
+	// Dealer
+	Genji,
+	Hanzo,
+	Para,
+	// Healer
+	Lucio,
+	Mersi,
+};
+
+enum EPacketType
+{
+	ECharInfo,
+	ECharMove,
+};
+
+struct PacketHeader
+{
+	WORD				m_Size;
+	EPacketType			m_PacketType;
+};
+
+struct PCharInfo : public PacketHeader
+{
+	ECharType			m_iCharSelect;
+	int					m_iPlayerNum;
+};
+
+// PacketUtil
+template<class T>
+void PacketEncode(char* buf, T& data)
+{
+	memcpy(buf, &data, sizeof(T));
+}
+template<class T>
+void PacketDecode(char*buf, T& data)
+{
+	memcpy(&data, buf, sizeof(T));
+}
+//
+int recvn(SOCKET S, char *buf, int len, int flags)
+{
+	int received{};
+	char* ptr = buf;
+	int left = len;	// BUFSIZE
+
+	while (left > 0)
+	{
+		received = recv(S, ptr, left, flags);
+		if (received == SOCKET_ERROR) return SOCKET_ERROR;
+		else if (received == 0) break;
+		cout << "[Received] : " << received << endl;
+		left -= received;
+		ptr += received;
+	}
+	return (len - left);
+}
 
 void main()
 {
@@ -32,37 +95,38 @@ void main()
 	// 통신 변수
 	SOCKET client_sock;
 	SOCKADDR_IN client_addr;
-	char buf[BUFSIZE + 1];
+	char buf[BUFSIZE + 1]{};
 	int addrlen{};
 	
-	// 크기 받기 데이터
-	int iLen{};
-
 	int retval{};
 	while (true)	// accept loop
-	{
+	{ 
 		addrlen = sizeof(client_addr);
 		client_sock = accept(listen_sock, (SOCKADDR*)&client_addr, &addrlen);
 		if (client_sock == INVALID_SOCKET) { cout << "accept() Error" << endl; break; }
-		//cout << addrlen << endl; system("cls");
-
 		cout << " [ Accept - IP : " << inet_ntoa(client_addr.sin_addr) << ", Port : " << ntohs(client_addr.sin_port) << " ] " << endl;
 
 		while (true)	// communication loop
 		{
 			// recv
-			retval = recv(client_sock, buf, BUFSIZE, 0);
+			retval = recvn(client_sock, buf, 16, 0);
 			cout << "[len] 받은 데이터 : " << retval << endl;
-			if (retval == SOCKET_ERROR) { cout << "recv()" << endl; break; }
-
-			//retval = recv(client_sock, buf, iLen , 0);
-			//cout << "[buf] 받은 데이터 : " << retval << endl;
-			//if (retval == SOCKET_ERROR) { cout << "recv()" << endl; break; }
+			if (retval == SOCKET_ERROR) { cout << "recvn()" << endl; break; }
 			else if (retval == 0) break; // client Quit
 
 			// Data Process Logic
-			buf[retval] = '\0';
-			cout << buf << endl;
+			PCharInfo CharInfo;
+			PacketDecode(buf, CharInfo);
+			switch (CharInfo.m_iCharSelect)
+			{
+				case Rine:  cout << "[" << CharInfo.m_iPlayerNum << "] " << "Rine" << endl; break;
+				case Diva:  cout << "[" << CharInfo.m_iPlayerNum << "] " << "Diva" << endl; break;
+				case Genji: cout << "[" << CharInfo.m_iPlayerNum << "] " << "Genji" << endl; break;
+				case Hanzo: cout << "[" << CharInfo.m_iPlayerNum << "] " << "Hanzo" << endl; break;
+				case Para:  cout << "[" << CharInfo.m_iPlayerNum << "] " << "Para" << endl; break;
+				case Lucio: cout << "[" << CharInfo.m_iPlayerNum << "] " << "Lucio" << endl; break;
+				case Mersi: cout << "[" << CharInfo.m_iPlayerNum << "] " << "Mersi" << endl; break;
+			}
 		}
 
 		closesocket(client_sock);
